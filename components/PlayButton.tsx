f73652b42e9fda6cd516a0bf8e13993f06adaecd
Derek Prieur <DPrieur123@gmail.com>
2023-08-09 10:11:49 -0400

    song card pause button 100 opacity always

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 const PlayButton = ({ isPlaying }: Props) => {
     const Icon = isPlaying ? FaPause : FaPlay
     return (
-        <button className='transition opacity-0 rounded-full flex items-center bg-green-500 p-4 drop-shadow-md translate-y-1/4 group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110'>
+        <button className={`transition rounded-full flex items-center bg-green-500 p-4 drop-shadow-md group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110 ${!isPlaying && 'opacity-0 translate-y-1/4'}`}>
             <Icon className='text-black' />
         </button>
     )
