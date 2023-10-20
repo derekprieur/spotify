249b05d652f313d9aee864a42352a2cecd485fb2
Derek Prieur <DPrieur123@gmail.com>
2023-10-20 14:50:00 -0400

    update

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -8,11 +8,11 @@ const useOnPlay = (songs: Song[]) => {
     const player = usePlayer()
     const authModal = useAuthModal()
     const subscribeModal = useSubscribeModal()
-    const {user, subscription} = useUser()
+    const { user, subscription } = useUser()
 
     const onPlay = (id: string) => {
-        if (!user) return authModal.onOpen()
-        if ( !subscription ) return subscribeModal.onOpen()
+        // if (!user) return authModal.onOpen()
+        // if ( !subscription ) return subscribeModal.onOpen()
 
         player.setId(id)
         player.setIds(songs.map(song => song.id))
